17aa04588fd8e3eb48f40f8b50c102112a2dfda0
Isaac Guzmán <mizook.21922@gmail.com>
2024-09-20 22:41:41 -0600

    what

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -4,7 +4,6 @@ function Hero() {
   return (
      <section className="relative h-screen bg-cover bg-center bg-no-repeat bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-8 lg:gap-16 px-4 lg:px-8 py-8 lg:py-16 " style={{ backgroundImage: "url('/space.jpg')" }}>
         <div className='flex flex-col items-center justify-center'>
-        <div className='items-center text-9xl'>
             <h1 className='font-bold items-center flex text-9xl text-white'>
             NEBULA 
             </h1>
