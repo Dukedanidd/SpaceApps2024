e79c04e65ef5c84d73effd496cf30a6af7666bdd
Isaac Guzmán <mizook.21922@gmail.com>
2024-09-20 22:36:02 -0600

    play btn

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Hero() {
   return (
      <section className="relative h-screen bg-cover bg-center bg-no-repeat bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-8 lg:gap-16 px-4 lg:px-8 py-8 lg:py-16 " style={{ backgroundImage: "url('/space.jpg')" }}>
-        <div className='items-center'>
+        <div className='flex flex-col items-center justify-center'>
             <h1 className='font-bold items-center flex text-9xl text-white'>
             NEBULA DRIFT
             </h1>
-            <button className='text-4xl rounded-xl bg-white'>
+            <button className='text-4xl justify-center bg-white px-3 py-1 my-2 rounded-xl'>
                Play
             </button>
         </div>
