ed8d102d933d2ff085a9c369f7ca2d704f19cf40
Duke <jd.daniel.contact@gmail.com>
2024-09-20 22:44:19 -0600

    chore: Update Hero component with new background image and title

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 function Hero() {
   return (
      <section className="relative h-screen bg-cover bg-center bg-no-repeat bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-8 lg:gap-16 px-4 lg:px-8 py-8 lg:py-16 " style={{ backgroundImage: "url('/space.jpg')" }}>
-        <div className='items-center text-9xl'>
-            <h1 className='font-bold items-center flex text-9xl text-white'>
+        <div className='items-center'>
+            <h1 id = 'nebula'className='font-bold items-center flex text-white text-cyan-200'>
             NEBULA 
             </h1>
-            <h1 className='mx-16 font-bold items-center flex text-9xl text-white'>
+            <div className='items-center text-center'>
+            <h1 id = 'drift'className='font-bold items-center text-9xl text-sky-300'>
             DRIFT
             </h1>
+            </div>
             <button className='text-4xl rounded-xl bg-white'>
                Play
             </button>
