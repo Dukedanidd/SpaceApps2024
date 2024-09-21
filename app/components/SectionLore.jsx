a83b10d03ddc1c499a38e3b042ee74ad548860f0
Duke <jd.daniel.contact@gmail.com>
2024-09-21 00:16:49 -0600

    chore: Update Hero component with new background image, title, and play button

--- app/components/SectionLore.jsx
+++ app/components/SectionLore.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 function SectionLore() {
-  return (
-    <section>
-         <div className="bg-gradient-to-r from-blue-500 via-purple-500 to-red-500 h-screen flex items-center justify-center">
-      <h1 className="text-white text-4xl">¡Bienvenido a mi sección!</h1>
-    </div>
-    </section>
-  )
+    return (
+        <section>
+            <div className="bg-gradient-to-r from-blue-500 via-purple-500 to-red-500 h-screen flex items-center justify-center">
+                <h1 className="text-white text-4xl">¡Bienvenido a mi sección!</h1>
+            </div>
+        </section>
+    )
 }
 
 export default SectionLore
