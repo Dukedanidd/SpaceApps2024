fb0beb7a024547c2770ecbc9dcd6cda85050783d
Duke <jd.daniel.contact@gmail.com>
2024-09-20 23:11:36 -0600

    chore: Update Hero component with new font sizes and alignment

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 
 function Hero() {
-  return (
-     <section className="relative h-screen bg-cover bg-center bg-no-repeat bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-8 lg:gap-16 px-4 lg:px-8 py-8 lg:py-16 " style={{ backgroundImage: "url('/space.jpg')" }}>
-        <div className='flex flex-col items-center justify-center'>
-            <h1 id = 'nebula'className='font-bold items-center flex text-white text-cyan-200'>
-            NEBULA 
-            </h1>
-            <div className='items-center text-center'>
-            <h1 id = 'drift'className='font-bold items-center text-9xl text-sky-300'>
-            DRIFT
-            </h1>
-            </div>
+   return (
+      <section className="relative h-screen bg-cover bg-center bg-no-repeat bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-8 lg:gap-16 px-4 lg:px-8 py-8 lg:py-16 " style={{ backgroundImage: "url('/space.jpg')" }}>
+         <div className='flex flex-col items-center justify-center'>
+            <p id='nebula' className='text-center text-cyan-200 text-9xl lg:text-9xl transform'>NEBULA</p>
+            <h1 id='drift' className='text-center text-sky-300 text-9xl lg:text-9xl font-bold'>DRIFT</h1>
             <button className='text-4xl px-4 py-2 my-2 rounded-xl bg-white'>
                Play
             </button>
