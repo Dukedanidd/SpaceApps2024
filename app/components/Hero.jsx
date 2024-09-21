6f829cf2848bc545738d4647629267a8626827c9
Duke <jd.daniel.contact@gmail.com>
2024-09-20 22:21:00 -0600

    chore: Update Hero component with new title and play button

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 function Hero() {
   return (
      <section className="relative h-screen bg-cover bg-center bg-no-repeat bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-8 lg:gap-16 px-4 lg:px-8 py-8 lg:py-16 " style={{ backgroundImage: "url('/space.jpg')" }}>
-        <div className='items-center'>
+        <div className='items-center text-9xl'>
             <h1 className='font-bold items-center flex text-9xl text-white'>
-            NEBULA DRIFT
+            NEBULA 
+            </h1>
+            <h1 className='mx-16 font-bold items-center flex text-9xl text-white'>
+            DRIFT
             </h1>
             <button className='text-4xl rounded-xl bg-white'>
                Play
