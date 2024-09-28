fa8db6a341bd83e3b98487f8c011c59568613a52
Carlos Ramos <57056947+CarlosIRamosV@users.noreply.github.com>
2024-09-27 18:06:42 -0600

    Camara

--- game/Assets/Scripts/CameraController.cs
+++ game/Assets/Scripts/CameraController.cs
@@ -1,41 +1,74 @@
 using UnityEngine;
+using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
-    public Transform playerBody;
-    private float xRotation = 0f;
-	private float moveSpeed = 0.5f;
-    private float scrollSpeed = 10f;
 
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
 
-    void Update()
-    {
-	
-	if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
-            transform.position += moveSpeed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        }
+	Vector2 _mouseAbsolute;
+	Vector2 _smoothMouse;
 
-	if (Input.GetAxis("Mouse ScrollWheel") != 0) {
-            transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
-        }
+	public Vector2 clampInDegrees = new Vector2 (360, 180);
+	public Vector2 sensitivity = new Vector2 (2, 2);
+	public Vector2 smoothing = new Vector2 (3, 3);
+	public Vector2 targetDirection;
+	public Vector2 targetCharacterDirection;
 
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+	// Assign this if there's a parent object controlling motion, such as a Character Controller.
+	// Yaw rotation will affect this object instead of the camera if set.
+	public GameObject characterBody;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+	void Start ()
+	{
+		// Set target direction to the camera's initial orientation.
+		targetDirection = transform.localRotation.eulerAngles;
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+		// Set target direction for the character body to its inital state.
+		if (characterBody)
+			targetCharacterDirection = characterBody.transform.localRotation.eulerAngles;
+	}
 
-        // Zoom in and out with the mouse scroll wheel
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        Camera.main.fieldOfView -= scroll * 20;
-        Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 20, 100);
-    }
+	void Update ()
+	{
+
+		// Allow the script to clamp based on a desired target value.
+		var targetOrientation = Quaternion.Euler (targetDirection);
+		var targetCharacterOrientation = Quaternion.Euler (targetCharacterDirection);
+
+		// Get raw mouse input for a cleaner reading on more sensitive mice.
+		var mouseDelta = new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
+
+		// Scale input against the sensitivity setting and multiply that against the smoothing value.
+		mouseDelta = Vector2.Scale (mouseDelta, new Vector2 (sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
+
+		// Interpolate mouse movement over time to apply smoothing delta.
+		_smoothMouse.x = Mathf.Lerp (_smoothMouse.x, mouseDelta.x, 1f / smoothing.x);
+		_smoothMouse.y = Mathf.Lerp (_smoothMouse.y, mouseDelta.y, 1f / smoothing.y);
+
+		// Find the absolute mouse movement value from point zero.
+		_mouseAbsolute += _smoothMouse;
+
+		// Clamp and apply the local x value first, so as not to be affected by world transforms.
+		if (clampInDegrees.x < 360)
+			_mouseAbsolute.x = Mathf.Clamp (_mouseAbsolute.x, -clampInDegrees.x * 0.5f, clampInDegrees.x * 0.5f);
+
+		var xRotation = Quaternion.AngleAxis (-_mouseAbsolute.y, targetOrientation * Vector3.right);
+		transform.localRotation = xRotation;
+
+		// Then clamp and apply the global y value.
+		if (clampInDegrees.y < 360)
+			_mouseAbsolute.y = Mathf.Clamp (_mouseAbsolute.y, -clampInDegrees.y * 0.5f, clampInDegrees.y * 0.5f);
+
+		transform.localRotation *= targetOrientation;
+
+		// If there's a character body that acts as a parent to the camera
+		if (characterBody) {
+			var yRotation = Quaternion.AngleAxis (_mouseAbsolute.x, characterBody.transform.up);
+			characterBody.transform.localRotation = yRotation;
+			characterBody.transform.localRotation *= targetCharacterOrientation;
+		} else {
+			var yRotation = Quaternion.AngleAxis (_mouseAbsolute.x, transform.InverseTransformDirection (Vector3.up));
+			transform.localRotation *= yRotation;
+		}
+	}
 }
