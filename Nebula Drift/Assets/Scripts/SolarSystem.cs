d827a39323918fb72468bcd46642f647c07cb1ef
Carlos Ramos <57056947+CarlosIRamosV@users.noreply.github.com>
2024-10-06 13:44:50 -0600

    Update Game

--- Nebula Drift/Assets/Scripts/SolarSystem.cs
+++ Nebula Drift/Assets/Scripts/SolarSystem.cs
@@ -6,10 +6,14 @@ public class SolarSystem : MonoBehaviour
 {
     private readonly float G = 100f;
     GameObject[] celelstials;
+    private int maxAsteroid;
+    private bool generated = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        maxAsteroid = 10000;
+        Generate();
         celelstials = GameObject.FindGameObjectsWithTag("Celestial");
         foreach (GameObject a in celelstials)
         {
@@ -49,5 +53,41 @@ public class SolarSystem : MonoBehaviour
         }
         // Slow simulation down
         Time.timeScale = 0.1f;
+    }
+
+    void Generate()
+    {
+        if (!generated)
+            for (int i = 0; i < maxAsteroid; i++)
+            {
+                // Create a new asteroid object
+                // Load the asteroid prefab
+                int random = Random.Range(1, 4);
+                GameObject asteroid = Instantiate(Resources.Load<GameObject>("Prefabs/Asteroid " + random));
+                int minRadius = 3141;
+                int maxRadius = 5086;
+                int randomRadius = Random.Range(minRadius, maxRadius);
+                float randomAngle = Random.Range(0, 2 * Mathf.PI);
+                float x = randomRadius * Mathf.Cos(randomAngle);
+                float z = randomRadius * Mathf.Sin(randomAngle);
+                asteroid.transform.localScale = new Vector3(1, 1, 1);
+                asteroid.transform.position = new Vector3(x, 0, z);
+                asteroid.tag = "Asteroid";
+
+                // Add collider to the asteroid
+                asteroid.AddComponent<SphereCollider>();
+
+            }
+        generated = true;
+    }
+
+    int RandomCooedinate()
+    {
+        int x = Random.Range(3141, 5086);
+        // Random negative or positive
+        if (Random.Range(0, 2) == 0)
+            x *= -1;
+        Debug.Log(x);
+        return x;
     }
 }
