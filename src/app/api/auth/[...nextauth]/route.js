517584a3f963b9d03ab52bcf64ecd6fd3bfc801a
Marki <markimarkiando@gmail.com>
2024-10-06 13:09:44 -0600

    Cmabios en inicio de sesion

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -8,12 +8,18 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-  //  GoogleProvider({
-    //  clientId: process.env.GOOGLE_CLIENT_ID,
-      //clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    //})
+  GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
     // ...add more providers here
   ],
+  callbacks: {
+    async session({ session, token }) {
+      session.user.id = token.sub; // Agrega el ID del usuario a la sesión
+      return session;
+    },
+  },
 }
 
 export const GET = NextAuth(authOptions)
