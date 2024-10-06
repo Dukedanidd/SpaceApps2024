c951a2ec3d897b1530f132a33f559c1c0b2c3e09
Duke <jd.daniel.contact@gmail.com>
2024-10-05 18:03:42 -0600

    Modified TypingEffect and SectionLore

--- src/components/SectionLore.jsx
+++ src/components/SectionLore.jsx
@@ -3,15 +3,12 @@ import TypingEffect from "./TypingEffect";
 
 function SectionLore() {
   return (
-    <section>
-      <div
-        className="bg-black h-screen flex items-center justify-center"
-        style={{ backgroundImage: "url('./stars.png')" }}
-      >
+    <section className="bg-black h-screen flex flex-col items-center justify-center overflow-y-hidden"
+    style={{ backgroundImage: "url('./stars.png')" }}>
+      <div className="max-w-full max-h-full"> {/* Limitar el ancho y alto del contenedor */}
         <h1 className="text-white text-4xl">
           <TypingEffect />
         </h1>
-        
       </div>
     </section>
   );
--- src/components/TypingEffect.jsx
+++ src/components/TypingEffect.jsx
@@ -9,53 +9,23 @@ export default function TypingEffect() {
   const typewriters = [
     {
       text: "Capitulo 1: El sueño de Astronautita Armstrong",
-      pause: 2000,
+      pause: 1000,
       next: 1,
     },
     {
       text: "Astronautita Armstrong, ingeniero de la NASA, siempre soñó con el espacio.<br/>Cuando lo seleccionaron para la Operación Estrella Brillante, su corazón<br/> se llenó de emoción. <br/>Tendría la oportunidad de desviar asteroides peligrosos.",
-      pause: 2500,
+      pause: 1500,
       next: 2,
     },
     {
       text: "Capitulo 2: Con DEX a Bordo",
-      pause: 2000,
+      pause: 1000,
       next: 3,
     },
     {
       text: "En la nave, conoció a DEX, un robot ingenioso.<br/> “¡Listo para la aventura!” dijo DEX, haciendo<br/> reír a Astronautita. Juntos, se prepararon <br/>para lo que vendría.",
-      pause: 2500,
+      pause: 1500,
       next: 4,
-    },
-    {
-      text: "Capítulo 3: La Amenaza",
-      pause: 2000,
-      next: 5,
-    },
-    {
-      text: 'Un asteroide enorme apareció en el radar. “¡Debemos actuar rápido!” gritó DEX. Idearon un plan para fragmentarlo con explosivos. Astronautita se sintió nervioso, pero recordó el colgante de estrella que su madre le dio.',
-      pause: 2500,
-      next: 6,
-    },
-    {
-      text: 'Capítulo 4: La Victoria',
-      pause: 2000,
-      next: 7,
-    },
-    {
-      text: 'Con precisión, activaron los explosivos. La explosión iluminó el espacio y los fragmentos se dispersaron, salvando a la Tierra.',
-      pause: 2500,
-      next: 8, 
-    },
-    {
-      text: 'Capitulo 5: Regreso a Casa',
-      pause: 2000,  
-      next: 9,
-    },
-    {
-      text: 'De regreso, la multitud aplaudía. Astronautita sonrió a DEX y dijo: “No podría haberlo hecho sin ti”. Aprendió que la verdadera aventura estaba en la amistad y el trabajo en equipo.',
-      pause: 2500,
-      next: 0, // Regresa al primer Typewriter
     },
   ];
 
@@ -71,31 +41,33 @@ export default function TypingEffect() {
         <h3>La Misión Estelar de Astronautita Armstrong y DEX</h3>
       </div>
       {typewriters.map((item, index) => (
-        index === currentIndex && (
-          <Typewriter
-            key={index}
-            options={{
-              autoStart: true,
-              loop: false,
-              delay: 40, // Velocidad de escritura
-              deleteSpeed: 20, // Velocidad de eliminación
-            }}
-            onInit={(typewriter) => {
-              typewriter
-                .typeString(item.text)
-                .callFunction(() => {
-                  console.log("String typed out!");
-                })
-                .pauseFor(item.pause) // Pausa después de escribir el texto
-                .callFunction(() => {
-                  console.log("Waiting before next string...");
-                  setTimeout(() => {
-                    handleNext(); // Llama a la función para avanzar al siguiente Typewriter después de 1.5 segundos
-                  }, 1500); // Espera 1500 ms antes de avanzar
-                })
-                .start();
-            }}
-          />
+        index <= currentIndex && ( // Cambiado de '===' a '<=' para mostrar todos los textos hasta el índice actual
+          <div style={{ marginBottom: '10px' }}> {/* Añadido un div con margen */}
+            <Typewriter
+              key={index}
+              options={{
+                autoStart: true,
+                loop: false,
+                delay: 0.70, // Velocidad de escritura
+                deleteSpeed: 20, // Velocidad de eliminación
+              }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString(item.text)
+                  .callFunction(() => {
+                    console.log("String typed out!");
+                  })
+                  .pauseFor(item.pause) // Pausa después de escribir el texto
+                  .callFunction(() => {
+                    console.log("Waiting before next string...");
+                    setTimeout(() => {
+                      handleNext(); // Llama a la función para avanzar al siguiente Typewriter después de 1.5 segundos
+                    }, 1500); // Espera 1500 ms antes de avanzar
+                  })
+                  .start();
+              }}
+            />
+          </div>
         )
       ))}
       <div dangerouslySetInnerHTML={{ __html: displayedText }} /> {/* Muestra el texto acumulado */}
