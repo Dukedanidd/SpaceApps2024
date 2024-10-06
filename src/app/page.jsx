2536e7c51f5e3b34c78a0b5f64cf33601f05cb03
Isaac Guzmán <mizook.21922@gmail.com>
2024-10-06 00:49:56 -0600

    fix home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,10 @@
 import Hero from "@components/Hero";
-import Login from "../components/Login";
 import Overlay from '../components/Overlay';
 
 export default function Home() {
   return <>
     <Overlay/>
     <Hero />
-    <Login />
   </>
 
     ;
