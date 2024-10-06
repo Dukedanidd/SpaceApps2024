6114bec78a4128c322f7446936daf3e3225950c6
Carlos Ramos <57056947+CarlosIRamosV@users.noreply.github.com>
2024-10-05 21:00:41 -0600

    Update Game

--- Nebula Drift/Assets/Scripts/Planets/Earth.cs
+++ Nebula Drift/Assets/Scripts/Planets/Earth.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Earth : MonoBehaviour
 {
+    private GameObject exitCollider;
+    private GameObject player;
     void Start()
     {
+        exitCollider = GameObject.Find("ExitCollider");
+        player = GameObject.Find("Player");
 
     }
 
     void Update()
     {
+        // Rotate the planet
         transform.Rotate(Vector3.up * 10 * Time.deltaTime);
+
+        // Detect if the camera is outside the exit collider
+        if (!exitCollider.GetComponent<Collider>().bounds.Contains(player.transform.position))
+        {
+            // Load the Solar System scene and set the camera position
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Solar System");
+        }
     }
 }
--- Nebula Drift/Assets/Scripts/Solar System/EarthSpace.cs
+++ Nebula Drift/Assets/Scripts/Solar System/EarthSpace.cs
@@ -7,32 +7,31 @@ public class EarthSpace : MonoBehaviour
 
     private GameObject infoCollider;
     private GameObject teletransporterCollider;
+    private GameObject player;
+    private GameObject infoPanel;
 
     void Start()
     {
         infoCollider = GameObject.Find("EarthInfoCollider");
         teletransporterCollider = GameObject.Find("EarthTeletransporterCollider");
+        player = GameObject.Find("Player");
+        infoPanel = player.transform.Find("Canvas").Find("InfoPanel").gameObject;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // Detect if the camera is inside the info collider
-        if (infoCollider.GetComponent<Collider>().bounds.Contains(Camera.main.transform.position))
+        if (infoCollider.GetComponent<Collider>().bounds.Contains(player.transform.position))
         {
-            // Show the info panel
-            //GameObject.Find("EarthInfoPanel").SetActive(true);
-            Debug.Log("Camera is inside the info collider");
-        }
-        else
-        {
-            // Hide the info panel
-            //GameObject.Find("EarthInfoPanel").SetActive(false);
-            Debug.Log("Camera is outside the info collider");
+            // Add Text to the Info Panel
+            Debug.Log("Inside Info Collider");
         }
 
         // Detect if the camera is inside the teletransporter collider
-        if (teletransporterCollider.GetComponent<Collider>().bounds.Contains(Camera.main.transform.position))
+        if (teletransporterCollider.GetComponent<Collider>().bounds.Contains(player.transform.position))
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene("Earth");
         }
